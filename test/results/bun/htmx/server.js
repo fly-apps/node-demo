b60af44b558d4519fbd865fd97817cb68d49e987
Sam Ruby <rubys@intertwingly.net>
2023-09-15 09:48:49 -0400

    add htmx

---
+++ test/results/bun/htmx/server.js
@@ -0,0 +1,41 @@
+// last known count
+let count = 0
+
+// Process requests based on pathname
+async function fetch(request) {
+  const { pathname } = new URL(request.url)
+
+  if (pathname === '/') {
+    return main(request)
+  } else if (await Bun.file(`public${pathname}`).exists()) {
+    return new Response(Bun.file(`public${pathname}`))
+  } else {
+    return new Response('Not found.', { status: 404 })
+  }
+}
+
+// Main page
+async function main(_request) {
+  // increment counter in counter.txt file
+  try {
+    count = parseInt(await Bun.file('counter.txt').text()) + 1
+  } catch {
+    count = 1
+  }
+
+  await Bun.write('counter.txt', count.toString())
+
+  // render HTML response
+  try {
+    let contents = await Bun.file('views/index.tmpl').text()
+    contents = contents.replace('@@COUNT@@', count.toString())
+
+    return new Response(contents, { status: 200, headers: { 'Content-Type': 'text/html' } })
+  } catch (error) {
+    return new Response(error + '\n', { status: 500, headers: { 'Content-Type': 'text/plain' } })
+  }
+}
+
+// Start web server on port 3000
+Bun.serve({ port: 3000, fetch })
+console.log('Server is listening on port 3000')
